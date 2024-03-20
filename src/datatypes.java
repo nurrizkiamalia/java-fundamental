2fbca7775c5fd75678975f3329a9b28ac6a89218
nurrizkiamalia <rizkiamel9@gmail.com>
2024-03-20 10:00:17 +0700

    feat: first initial

---
+++ src/datatypes.java
@@ -0,0 +1,30 @@
+public class datatypes {
+    public static void main(String[] args) {
+//        int age;
+//        age = 25;
+//
+//        System.out.println(age);
+//
+//        int[] ages = {25, 30, 23};
+//
+//        double[] salaries = {50000.00, 60000.00, 70000.00};
+//        char[] grades = {'A','B','C'};
+//        boolean[] maritalStatus = {true, false, true};
+//
+//        for (int i = 0; i < ages.length; i++){
+//            System.out.println("age: "+ages[i]+", salary: "+salaries[i]+ ", grades: "+grades[i]+ ", marital status: "+maritalStatus[i]);
+//        }
+
+//        int numberA = 20;
+//        long longNumber = numberA;
+//
+//        long longNumber = (long) numberA;
+//
+//        System.out.println(longNumber);
+
+        int num = 10;
+        double result = Math.pow(num, 2);
+        System.out.println(result);
+
+    }
+}
---
+++ src/exercise1.java
@@ -0,0 +1,95 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.lang.Math.*;
+import java.time.*;
+
+public class exercise1 {
+
+    private static int calculateArea(int length, int width){
+        int area = length * width;
+        return area;
+    }
+
+    public static void calculateAreaofCircle(int radius){
+        int diameter = radius * 2;
+        double circumference = diameter * Math.PI;
+        double area = Math.pow(circumference, 2) / (4 * Math.PI);
+
+        System.out.println("Diameter: "+diameter);
+        System.out.println("Circumference: "+circumference);
+        System.out.println("Area of circle: "+area);
+    }
+
+    public static int calculateTriangleAngles(int angles1, int angles2){
+        int triangle = 180 - (angles1+angles2);
+        return triangle;
+    }
+
+    public static int dateDifferences(String d1, String d2){
+//        Date time pattern formatter
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date1 = LocalDate.parse(d1, DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate date2 = LocalDate.parse(d2, DateTimeFormatter.ISO_LOCAL_DATE);
+        Duration diff = Duration.between(date1.atStartOfDay(), date2.atStartOfDay());
+//        long dayDifference = ChronoUnit.Days.between(date1, date2);
+        int diffDays = (int) diff.toDays();
+        return diffDays;
+    }
+
+    public static void firstLetterInitial(String name){
+        name = ' ' + name;
+
+        int strLen = name.length();
+        System.out.print("Short of name: ");
+        for (int i = 0; i < strLen; i++) {
+            if (name.charAt(i) == (' '))
+                System.out.print(Character.toUpperCase(name.charAt(i + 1)));
+        }
+    }
+
+    public static void main(String[] args) {
+//        Number 1
+        Scanner in = new Scanner(System.in);
+        System.out.print("Length: ");
+        int length = in.nextInt();
+        System.out.print("Width: ");
+        int width = in.nextInt();
+
+        System.out.println("Wide area: "+calculateArea(length,width));
+        System.out.println();
+
+        in.nextLine();
+
+//        Number 2
+        System.out.print("Radius: ");
+        int radius = in.nextInt();
+        calculateAreaofCircle(radius);
+        System.out.println();
+
+        in.nextLine();
+
+//        Number 3
+        System.out.print("Angles 1: ");
+        int angles1 = in.nextInt();
+        System.out.print("Angles 2: ");
+        int angles2 = in.nextInt();
+        System.out.println("Angles of triangle: "+calculateTriangleAngles(angles1,angles2));
+        System.out.println();
+
+        in.nextLine();
+
+//        Number 4
+        System.out.print("Date before: ");
+        String date1 = in.nextLine();
+        System.out.print("Date after: ");
+        String date2 = in.next();
+        System.out.println("Date Difference: "+dateDifferences(date1, date2)+" days");
+        System.out.println();
+        in.nextLine();
+        in.close();
+//        Number 5
+        System.out.print("Name: ");
+        String name = in.nextLine();
+        firstLetterInitial(name);
+    }
+}
